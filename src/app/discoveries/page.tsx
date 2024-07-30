bb3d34f01d051d7256cd0d16bd1ac88e56c80aef
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-07-30 07:54:02 +0200

    Show initial and user-created recipes in separate lists

--- src/app/discoveries/page.tsx
+++ src/app/discoveries/page.tsx
@@ -7,22 +7,48 @@ const getRecipes = new GetRecipes(recipeRepository);
 export default async function Discoveries() {
   const recipes = await getRecipes.run();
 
+  const initialRecipes = recipes.filter(({ isInitial }) => isInitial);
+  const userCreatedRecipes = recipes.filter(({ isInitial }) => !isInitial);
+
   return (
-    <ul className="w-full grid gap-4">
-      {recipes.map((recipe) => (
-        <li key={recipe.id} className="w-full p-3 rounded bg-secondary grid">
-          {recipe.isDiscovered ? (
-            <>
+    <ul className="w-full grid lg:grid-cols-2 gap-8">
+      <div className="w-full grid gap-4">
+        Recettes classiques :
+        {initialRecipes.map((recipe) => (
+          <li key={recipe.id} className="w-full p-3 rounded bg-secondary grid">
+            {recipe.isDiscovered ? (
+              <>
+                <div>{recipe.name}</div>
+                <div className="text-sm text-muted-foreground">
+                  {recipe.ingredients.map(({ name }) => name).join(", ")}
+                </div>
+              </>
+            ) : (
+              <div className="text-muted-foreground italic">À découvrir…</div>
+            )}
+          </li>
+        ))}
+      </div>
+      <div className="w-full grid gap-4 content-baseline">
+        Recettes créées :
+        {userCreatedRecipes.length ? (
+          userCreatedRecipes.map((recipe) => (
+            <li
+              key={recipe.id}
+              className="w-full p-3 rounded bg-secondary grid"
+            >
               <div>{recipe.name}</div>
               <div className="text-sm text-muted-foreground">
                 {recipe.ingredients.map(({ name }) => name).join(", ")}
               </div>
-            </>
-          ) : (
-            <div className="text-muted-foreground italic">À découvrir…</div>
-          )}
-        </li>
-      ))}
+            </li>
+          ))
+        ) : (
+          <div className="text-muted-foreground italic">
+            Aucune recette créée pour l'instant.
+          </div>
+        )}
+      </div>
     </ul>
   );
 }
