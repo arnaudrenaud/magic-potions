85fcf0a787640ab76a07c0d737daf79681fd1b34
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-07-30 22:41:30 +0200

    Improve semantics of Discoveries page

--- src/app/discoveries/page.tsx
+++ src/app/discoveries/page.tsx
@@ -11,44 +11,51 @@ export default async function Discoveries() {
   const userCreatedRecipes = recipes.filter(({ isInitial }) => !isInitial);
 
   return (
-    <ul className="w-full grid lg:grid-cols-2 gap-8">
-      <div className="w-full grid gap-4">
-        Potions classiques :
-        {initialRecipes.map((recipe) => (
-          <li key={recipe.id} className="w-full p-3 rounded bg-secondary grid">
-            {recipe.isDiscovered ? (
-              <>
-                <div>{recipe.name}</div>
-                <div className="text-sm text-muted-foreground">
-                  {recipe.ingredients.map(({ name }) => name).join(", ")}
-                </div>
-              </>
-            ) : (
-              <div className="text-muted-foreground italic">À découvrir…</div>
-            )}
-          </li>
-        ))}
-      </div>
-      <div className="w-full grid gap-4 content-baseline">
-        Potions créées :
-        {userCreatedRecipes.length ? (
-          userCreatedRecipes.map((recipe) => (
+    <main className="w-full grid lg:grid-cols-2 gap-8">
+      <div className="space-y-4">
+        <div>Potions classiques :</div>
+        <ul className="w-full grid gap-4">
+          {initialRecipes.map((recipe) => (
             <li
               key={recipe.id}
               className="w-full p-3 rounded bg-secondary grid"
             >
-              <div>{recipe.name}</div>
-              <div className="text-sm text-muted-foreground">
-                {recipe.ingredients.map(({ name }) => name).join(", ")}
-              </div>
+              {recipe.isDiscovered ? (
+                <>
+                  <div>{recipe.name}</div>
+                  <div className="text-sm text-muted-foreground">
+                    {recipe.ingredients.map(({ name }) => name).join(", ")}
+                  </div>
+                </>
+              ) : (
+                <div className="text-muted-foreground italic">À découvrir…</div>
+              )}
             </li>
-          ))
+          ))}
+        </ul>
+      </div>
+      <div className="space-y-4">
+        <div>Potions créées :</div>
+        {userCreatedRecipes.length ? (
+          <ul className="w-full grid gap-4">
+            {userCreatedRecipes.map((recipe) => (
+              <li
+                key={recipe.id}
+                className="w-full p-3 rounded bg-secondary grid"
+              >
+                <div>{recipe.name}</div>
+                <div className="text-sm text-muted-foreground">
+                  {recipe.ingredients.map(({ name }) => name).join(", ")}
+                </div>
+              </li>
+            ))}
+          </ul>
         ) : (
           <div className="text-muted-foreground italic">
             Aucune recette créée pour l'instant.
           </div>
         )}
       </div>
-    </ul>
+    </main>
   );
 }
